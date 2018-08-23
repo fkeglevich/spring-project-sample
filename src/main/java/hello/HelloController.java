c32fc7b3f18ba93d4340f40d9837534c8c027abc
Flávio Keglevich <fkeglevich@gmail.com>
2018-08-23 16:58:17 -0300

    Updando código do M03

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,15 +2,20 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.joda.time.LocalTime;
+import org.joda.time.*;
+import org.joda.time.format.*;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/data")
     public String index() {
-		LocalTime currentTime = new LocalTime();
-        return "Greetings from Spring Boot!" + currentTime; //"<html><body>Greetings from <i>Spring Boot!</i></body></html>";
+		
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH'h'mm");
+		
+		DateTime currentTime = new DateTime();
+		
+        return "Greetings from Spring Boot! " + formatter.print(currentTime); //"<html><body>Greetings from <i>Spring Boot!</i></body></html>";
     }
 
 }
