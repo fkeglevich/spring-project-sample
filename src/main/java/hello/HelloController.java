5810a55e57f0dcd9158acae3eb9c7c0e4f99a801
Flávio Keglevich <fkeglevich@gmail.com>
2018-08-23 16:41:14 -0300

    Updando código do M02

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,13 +2,15 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.joda.time.LocalTime;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/data")
     public String index() {
-        return "Greetings from Spring Boot!"; //"<html><body>Greetings from <i>Spring Boot!</i></body></html>";
+		LocalTime currentTime = new LocalTime();
+        return "Greetings from Spring Boot!" + currentTime; //"<html><body>Greetings from <i>Spring Boot!</i></body></html>";
     }
 
 }
